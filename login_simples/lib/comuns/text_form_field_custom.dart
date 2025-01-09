f6bc6a63e33eecd4d42c861db06da87036fb860c
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-01-09 19:18:17 -0300

    create Widget textFormFieldCustom

---
+++ login_simples/lib/comuns/text_form_field_custom.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+Widget textFormFieldCustom(
+  TextEditingController controller,
+  String label,
+  String hint,
+  bool showObscured,
+  bool? obscured,
+  bool showIcon,
+  IconData? icon,
+  VoidCallback? callBack,
+  String errorText,
+  Function (String value) function
+) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(
+      horizontal: 15,
+      vertical: 10,
+    ),
+    child: TextFormField(
+      onChanged:function,
+      style: TextStyle(color: Colors.black),
+      obscureText: showObscured ? obscured! : false,
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.black),
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        suffixIcon:
+            showIcon ? IconButton(onPressed: callBack, icon: Icon(icon)) : null,
+      ),
+    ),
+  );
+}
