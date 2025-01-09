d3fd85acdfdeca11f9fedca816286f2b266e2fbb
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-01-09 19:32:34 -0300

    create colors

---
+++ login_simples/lib/comuns/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF4A90E2);
+  static const Color buttons = Color(0xFF4A90E2);
+  static const Color backGround = Color(0xFFEAF6FF);
+  static const Color textPrimary = Color(0xFF4A4A4A);
+  static const Color textSecondary = Color(0xFF9B9B9B);
+  static const Color border = Color(0xFFD9D9D9);
+  static const Color error =Color(0xFFE57373);
+  static const Color success =Color(0xFF81C784);
+}
